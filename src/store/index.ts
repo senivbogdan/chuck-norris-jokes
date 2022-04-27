1f256637bc6e956c7a65a0038c2f2a7d3e18addc
senivbogdan <senivbogdan@github.com>
2022-04-27 18:11:31 +0300

    com

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,10 @@
 import {applyMiddleware, createStore} from "redux";
 import thunk from "redux-thunk";
 import {rootReducer} from "./reducer"
+// import { legacy_createStore as createStore} from 'redux'
 
+export const persistedState = localStorage.getItem("jokes")
+    ? JSON.parse(localStorage.getItem("jokes") || "{}")
+    : {}
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
